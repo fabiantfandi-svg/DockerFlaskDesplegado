a04c23bcf99972d954f901812d8ef6dea5bc8685
Fabian Torres <you@gmail.com>
2026-08-26 14:00:24 +0000

    Cambios para DevSecOps

--- sample_app.py
+++ sample_app.py
@@ -1,12 +1,18 @@
-import pymysql
+import os
 from flask import Flask
+import pymysql
 
 sample = Flask(__name__)
 
 @sample.route("/")
 def home():
     try:
-        conn = pymysql.connect(host='servidor-bd-ejemplo', user='root', password='sena123', database='082_db')
+        conn = pymysql.connect(
+            host=os.getenv('SERVER_HOST', 'servidor-bd-ejemplo'),
+            user=os.getenv('SERVER_USER', 'root'),
+            password=os.getenv('SERVER_SSH_KEY'),
+            database=os.getenv('MYSQL_DATABASE', 'cba_backend_db')
+        )
         conn.close()
         db_status = "Conexión exitosa a la base de datos"
     except Exception as e:
@@ -15,4 +21,7 @@ def home():
     return f"<h1>Bienvenido a mi aplicación Flask</h1><p>{db_status}</p>"
 
 if __name__ == "__main__":
-    sample.run(host='0.0.0.0', port=5050, debug=True)
+    host_ip = os.getenv("FLASK_RUN_HOST", "127.0.0.1")
+    modo_debug = os.getenv("FLASK_DEBUG", "false").lower() == "true"
+    
+    sample.run(host=host_ip, port=5050, debug=modo_debug)  
--- test_sample.py
+++ test_sample.py
@@ -1,16 +1,6 @@
-import pytest
-from sample_app import sample as app
+from sample_app import sample
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
-
-def test_read_main_success(client):
-    """Valida que la ruta principal devuelva un código HTTP 200 OK."""
-    response = client.get('/')
-    assert response.status_code == 200
-    
-# def test_ejemplo_basico():
- # assert 1 + 1 == 2
+def test_home_status_200():
+    cliente = sample.test_client()
+    respuesta = cliente.get('/')
+    assert respuesta.status_code == 200  # nosec B101
